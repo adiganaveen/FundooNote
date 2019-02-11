c389cd6b535ce8d0ea73dbfbeaaac9d70d49be14
adiganaveen <adiga.naveen77@gmail.com>
2019-02-11 19:29:44 +0530

    first commit

--- FundooNote/src/main/java/com/bridgelabz/spring/Utility/Validation.java
+++ FundooNote/src/main/java/com/bridgelabz/spring/Utility/Validation.java
@@ -7,11 +7,6 @@ import org.springframework.validation.Validator;
 import com.bridgelabz.spring.model.User;
 
 public class Validation implements Validator {
-	static final String REGEX_NAME ="^[a-zA-Z]{3,20}$";
-	static final String REGEX_EMAIL_ID = "^[a-z0-9._%-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
-	static final String REGEX_PASSWORD = "^[a-zA-Z0-9]{3,8}$";
-	static final String REGEX_MOBILENUMBER = "^[1-9]{1}[0-9]{9}$";
-
 	@Override
 	public boolean supports(Class<?> userClass) {
 		return User.class.equals(userClass);
@@ -20,27 +15,24 @@ public class Validation implements Validator {
 	@Override
 	public void validate(Object target, Errors errors) {
 		User user = (User) target;
-		if(!(user.getName().matches(REGEX_NAME)))
-		{
-			errors.rejectValue("name", "symbolsPresent",new Object[]{"'name'"},"name can't be symbols");
+		if (!(user.getName().matches("^[a-zA-Z]{3,20}$"))) {
+			errors.rejectValue("name", "symbolsPresent", new Object[] { "'name'" }, "name can't be symbols");
 		}
-		if(!(user.getEmailId().matches(REGEX_EMAIL_ID)))
-		{
-			errors.rejectValue("emailId", "symbolsPresent",new Object[]{"'emailId'"},"emailId can't be symbols");
+		if (!(user.getEmailId().matches("^[a-z0-9._%-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"))) {
+			errors.rejectValue("emailId", "symbolsPresent", new Object[] { "'emailId'" }, "emailId can't be symbols");
 		}
-		if(!(user.getPassword().matches(REGEX_PASSWORD)))
-		{
-			errors.rejectValue("password", "symbolsPresent",new Object[]{"'password'"},"password can't be symbols");
+		if (!(user.getPassword().matches("^[a-zA-Z0-9]{3,8}$"))) {
+			errors.rejectValue("password", "symbolsPresent", new Object[] { "'password'" },
+					"password can't be symbols");
 		}
-		if(!(String.valueOf(user.getMobileNumber())).matches(REGEX_MOBILENUMBER))
-		{
-			errors.rejectValue("mobileNumber", "symbolsPresent",new Object[]{"'mobileNumber'"},"mobile number can't be symbols");
+		if (!(String.valueOf(user.getMobileNumber())).matches("^[1-9]{1}[0-9]{9}$")) {
+			errors.rejectValue("mobileNumber", "symbolsPresent", new Object[] { "'mobileNumber'" },
+					"mobile number can't be symbols");
 		}
-		
 		ValidationUtils.rejectIfEmpty(errors, "name", "name.required");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "password.required");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "emailId","emailId.required");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "mobileNumber","mobileNumber.required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "emailId", "emailId.required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "mobileNumber", "mobileNumber.required");
 	}
 
 }
